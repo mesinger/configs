faa9b80f01a2401d735931695a420e2d2179730e
mesinger <rico.exe@pm.me>
2024-01-18 15:44:38 +0100

    add k pod forward alias

--- shell/fish/squer.fish
+++ shell/fish/squer.fish
@@ -3,3 +3,14 @@ function onepwd
     eval "op item get $id --field password | tr -d '\n' | pbcopy"
     echo "Copied to clipboard"
 end
+
+function k-fwd-sync
+  set -l pod_id (kubectl get pods -n backend | grep "sync" | awk '{print $1}' | tr -d '\n')
+  eval "kubectl port-forward -n backend $pod_id 38080:8080"
+end
+
+function k-fwd-be
+  set -l pod_id (kubectl get pods -n backend | grep "backend" | grep -v "sync" | awk '{print $1}' | tr -d '\n')
+  eval "kubectl port-forward -n backend $pod_id 38080:8080"
+end
+
